e1109cc0652a09a2bbe8c2e2bdc21083d7b612b4
Fedor <fedoravdeev3@gmail.com>
2023-07-21 19:23:37 +0700

    add login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import VisualizationFirstView from "../views/VisualizationFirstView.vue";
-import VisualizationSecondView from "../views/VisualizationSecondView.vue";
-import ErrorView from "../views/ErrorView.vue";
-import SettingsView from "../views/SettingsView.vue";
-import NewLavelView from "../views/NewLavelView.vue";
-import AppView from "../views/AppView.vue";
-import StreamingView from "../views/StreamingView.vue";
+import VisualizationFirstView from "@/views/VisualizationFirstView.vue";
+import VisualizationSecondView from "@/views/VisualizationSecondView.vue";
+import ErrorView from "@/views/ErrorView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+import NewLavelView from "@/views/NewLavelView.vue";
+import AppView from "@/views/AppView.vue";
+import StreamingView from "@/views/StreamingView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     redirect: { name: "visualization-first" },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
     path: "/visualization-first",
