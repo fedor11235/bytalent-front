10a40489213c94dd7a8d95892cd1edc70b8dbc5d
Fedor <fedoravdeev3@gmail.com>
2023-07-21 20:47:24 +0700

    netlify config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/]
+    }
+  },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
   ? '/bytalent-front/'
